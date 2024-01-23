fe16a45576603d90b8a8cf54111675fa3a4eeb85
Luisa Gomez <luisa.gomez@pucp.pe>
2024-01-23 14:43:51 -0500

    aar

--- prueba-image.py
+++ prueba-image.py
@@ -13,42 +13,33 @@ imagen_zoila = "ZOILA.png"
 imagen_team_zoila = "Team Zoila.JPG"
 # Definimos la ruta de la carpeta donde se encuentran las imágenes de la base de datos de Zoila
 ##folder_zoila = "C:\\Users\\u_humanidades\\Desktop\\proyecto\\ID-Item" (ruta lab)
-folder_zoila = "C:\\Users\\aml\\Desktop\\proyecto\\ID-Item"
+folder_zoila = "C:\\Users\\u_humanidades\\Desktop\\proyecto\\ID-Item"
 
 
 def load_images_and_rgb(folder_path):
-    images_data = []  # Almacenará información sobre cada imagen
-    rgb_data = []     # Almacenará los rasgos RGB de cada imagen
+    images_data = []
+    rgb_data = []
 
     for filename in os.listdir(folder_path):
         if filename.endswith(('.JPG', '.jpeg', '.PNG')):
             file_path = os.path.join(folder_path, filename)
             img = Image.open(file_path)
 
-            # Obtener el ID-Item (nombre del archivo sin extensión)
             id_item = os.path.splitext(filename)[0]
 
-            # Agregar información al DataFrame de imágenes
             images_data.append({'ID-Item': id_item, 'image': img})
-
-            # Obtener los rasgos RGB de la imagen
             rgb_values = list(img.getdata())
             rgb_data.append({'ID-Item': id_item, 'RGB': rgb_values})
 
-    # Crear DataFrames
     images_df = pd.DataFrame(images_data)
     rgb_df = pd.DataFrame(rgb_data)
 
-    # Establecer el ID-Item como índice en el DataFrame de imágenes
     images_df.set_index('ID-Item', inplace=True)
     rgb_df.set_index('ID-Item', inplace=True)
 
     return images_df, rgb_df
 
 def open_image(images_df, id_item):
-    """
-    Abre la imagen correspondiente al ID-Item especificado.
-    """
     if id_item in images_df.index:
         img = images_df.loc[id_item, 'image']
         st.image(img, caption=f"ID-Item: {id_item}", use_column_width=True)
@@ -61,13 +52,15 @@ images_df, rgb_df = load_images_and_rgb(folder_zoila)
 # Añadir un widget de búsqueda en Streamlit
 search_query = st.text_input("Buscar por ID-Item:")
 
-# Filtrar el DataFrame por ID-Item
-filtered_images_df = images_df[images_df.index.str.contains(search_query, case=False, na=False)]
+# Verificar si se ha ingresado una consulta de búsqueda
+if search_query:
+    # Filtrar el DataFrame por ID-Item
+    filtered_images_df = images_df[images_df.index.str.contains(search_query, case=False, na=False)]
 
-# Mostrar los resultados
-if not filtered_images_df.empty:
-    # Mostrar imágenes correspondientes al resultado de la búsqueda
-    for index, row in filtered_images_df.iterrows():
-        st.image(row['image'], caption=f"ID-Item: {index}", use_column_width=True)
-else:
-    st.warning("No se encontraron resultados para la búsqueda.")
+    # Mostrar los resultados
+    if not filtered_images_df.empty:
+        # Mostrar imágenes correspondientes al resultado de la búsqueda
+        for index, row in filtered_images_df.iterrows():
+            open_image(images_df, index)
+    else:
+        st.warning("No se encontraron resultados para la búsqueda.")
